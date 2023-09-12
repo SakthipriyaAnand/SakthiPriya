88fc73d8673f6c234c69c606d398b52ba1d77d9c
SakthipriyaAnand <bsakthipriya12793@gmail.com>
2023-09-12 11:43:43 +0530

    Initializing

---
+++ src/New/Day.java
@@ -0,0 +1,9 @@
+package New;
+
+public class Day {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module NewTesting {
+}
